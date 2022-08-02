2ef8178b3520836d792a0f2bdca25742a92a056a
rich <108325206+crich28@users.noreply.github.com>
2022-08-02 01:44:32 -0700

    code

---
+++ test.py
@@ -0,0 +1,17 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.espn.com/mlb/playerratings'
+result = requests.get(url)
+doc = BeautifulSoup(result.text, 'html.parser')
+body = doc.find_all('td')
+
+for tr in body:
+    for td in tr.contents[1:6]:
+        print(td.string)
+        print()
+    
+rank ={}
+print()
