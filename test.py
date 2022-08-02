6e1396dc91befa62c88e5842285b845ab2faf746
rich <108325206+crich28@users.noreply.github.com>
2022-08-02 03:05:02 -0700

    csv writer+list

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-
+import csv
+from fileinput import filename
+from tokenize import Name
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,9 +10,22 @@ result = requests.get(url)
 doc = BeautifulSoup(result.text, 'html.parser')
 body = doc.find_all('td')
 
-for tr in body:
+ranks =[]
+for tr in body[13:]:
     for td in tr.contents[1:6]:
-        print(td.string)
-        print()
-    
+        strings=td.string
+        ranks.append(strings)
 
+
+def splitter(list, n):
+    for i in range(0,len(list),n):
+        yield list[i:i+n]     
+
+stats = list(splitter(ranks,5))
+
+tags = ['Player', 'Team', 'Pos', 'Rank', 'Ratings']  
+filename = 'mlbrankings.csv'      
+with open(filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(tags)
+    csvwriter.writerows(stats)
