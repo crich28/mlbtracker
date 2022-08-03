69ec33fec93bcb61181a4c4287112b58c0a32326
rich <108325206+crich28@users.noreply.github.com>
2022-08-03 04:33:26 -0700

    var updates

--- mlbdata.py
+++ mlbdata.py
@@ -1,35 +1,33 @@
 
-from importlib.resources import contents
+
 import requests
 from bs4 import BeautifulSoup
 import csv
 
 url = 'https://www.espn.com/mlb/playerratings'
 result = requests.get(url)
-doc = BeautifulSoup(result.text, 'html.parser')
-body = doc.find_all('table')
+body = BeautifulSoup(result.text, 'html.parser')
+tables = body.find_all('table')
 
 data =[]   
     
-trs = body[2].find_all('tr')
+trs = tables[2].find_all('tr')
 for tr in trs[3:]:
-    td = tr.find_all("td")
-    for line in td[1:6]:
-        line = line.string
-        data.append(line)
+    tds = tr.find_all("td")
+    row = []
+    for td in tds[1:6]:
+        td = td.string
+        row.append(td)
+    data.append(row)
     
 
 
     
-def spliiter(list,n):
-    for i in range(0,len(list),n):
-        yield list[i:i+n]
 
-stats = (list(spliiter(data,5)))
 
 tags = ['Player', 'Team', 'Pos', 'Rank', 'Ratings']  
 filename = 'mlbrankings.csv'      
 with open(filename, 'w') as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(tags)
-    csvwriter.writerows(stats)
+    csvwriter.writerows(data)
